df5594cb9f4e1a3d028dd1f5ca74638ec4cb7495
wesbrewer23 <45601252+wesbrewer23@users.noreply.github.com>
2018-12-04 13:50:10 -0600

    RankedGradeBook Override CalculateStatistic

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -30,5 +30,21 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationExcecption("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else
+                base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationExcecption("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else
+                base.CalculateStudentStatistics(name);
+        }
     }
 }
