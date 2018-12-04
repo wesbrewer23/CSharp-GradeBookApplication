b46075514289977a7110a268cfc55c7498cde2f0
wesbrewer23 <45601252+wesbrewer23@users.noreply.github.com>
2018-12-04 13:24:47 -0600

    Override RankedGradeBook GetLetterGrade

---
+++ GradeBook/GradeBooks/InvalidOperationExcecption.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace GradeBook.GradeBooks
+{
+    [Serializable]
+    internal class InvalidOperationExcecption : Exception
+    {
+        public InvalidOperationExcecption()
+        {
+        }
+
+        public InvalidOperationExcecption(string message) : base(message)
+        {
+        }
+
+        public InvalidOperationExcecption(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidOperationExcecption(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -6,7 +8,27 @@ namespace GradeBook.GradeBooks
     {
         public RankedGradeBook(string name) : base(name)
         {
-            Type = Enums.GradeBookType.Ranked;
+            Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationExcecption("Ranked grading requires at least 5 students.");
+
+            var threshold = (int)Math.Ceiling(Students.Count * .2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[threshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(threshold * 3)- 1] <= averageGrade)
+                return 'C';
+            else  if (grades[(threshold * 4)- 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
