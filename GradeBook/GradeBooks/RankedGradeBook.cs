6ee026501102213f9abcc8c833da33eadba67cfe
wesbrewer23 <45601252+wesbrewer23@users.noreply.github.com>
2018-12-04 13:54:34 -0600

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -34,17 +34,20 @@ namespace GradeBook.GradeBooks
         public override void CalculateStatistics()
         {
             if (Students.Count < 5)
-                throw new InvalidOperationExcecption("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
-            else
-                base.CalculateStatistics();
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+                            
+             base.CalculateStatistics();
         }
 
         public override void CalculateStudentStatistics(string name)
         {
             if (Students.Count < 5)
-                throw new InvalidOperationExcecption("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
-            else
-                base.CalculateStudentStatistics(name);
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            base.CalculateStudentStatistics(name);
         }
     }
 }
